3d8a93eb99449fc565a4cc68f27b3ce998775b4a
Gavin D'Mello <gavin.dmello@points.com>
2018-06-27 13:18:39 -0400

    upograded example to work with templates pkg.

--- templates.go
+++ templates.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"text/template"
+	"github.com/Masterminds/sprig"
+	"html/template"
 	"bytes"
 )
 
@@ -13,12 +14,13 @@ Based on http://technosophos.com/2016/03/29/go-quickly-50-template-functions-fro
 func main() {
 	// The template
 	tpl := "Hello, {{.Name}}"
-
+	
 	// The values to pass to the template
 	vars := map[string]string{"Name": "World"}
-
+	
 	// Compile the template
-	t := template.Must(template.New("example").Parse(tpl))
+	// t := template.Must(template.New("example").Parse(tpl))
+	t := template.Must(template.New("too").Funcs(sprig.FuncMap()).Parse(tpl))
 
 	// Run the template, copying the output to the buffer.
 	var b bytes.Buffer
