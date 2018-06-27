332c06939ffbf569148bd3d7363640c51f89766c
Gavin D'Mello <gavin.dmello@points.com>
2018-06-27 11:46:31 -0400

    initial draft of go templates test for helm template verification.

---
+++ templates.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"text/template"
+	"bytes"
+)
+
+/* 
+Based on http://technosophos.com/2016/03/29/go-quickly-50-template-functions-from-sprig.html
+
+*/
+
+func main() {
+	// The template
+	tpl := "Hello, {{.Name}}"
+
+	// The values to pass to the template
+	vars := map[string]string{"Name": "World"}
+
+	// Compile the template
+	t := template.Must(template.New("example").Parse(tpl))
+
+	// Run the template, copying the output to the buffer.
+	var b bytes.Buffer
+	err := t.Execute(&b, vars)
+	if err != nil {
+			panic(err)
+	}
+	print(b.String())
+}
